38b8d28be313bd107c5d549dfa48f83683d939c0
thedevbrian1 <brayomwas95@gmail.com>
2023-10-28 08:24:39 +0300

    m

--- app/session.server.js
+++ app/session.server.js
@@ -1,7 +1,4 @@
 import { createCookieSessionStorage } from "@remix-run/node";
-// import invariant from "tiny-invariant";
-
-// invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
 
 export const sessionStorage = createCookieSessionStorage({
     cookie: {
